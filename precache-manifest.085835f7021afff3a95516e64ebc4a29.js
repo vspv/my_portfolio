95e9b9ed582bb530cf44ded7a2bc98eea4a678f0
vspv <seva--seva@ukr.net>
2020-06-01 13:41:13 +0300

    Updates

---
+++ precache-manifest.085835f7021afff3a95516e64ebc4a29.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d397d5e6c255bdc9d20307b22eadfb8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "353eeed7801141672e0b",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "ea64b8b88d9fa61c777f",
+    "url": "/portfolio/static/css/main.ed6b8eee.chunk.css"
+  },
+  {
+    "revision": "353eeed7801141672e0b",
+    "url": "/portfolio/static/js/2.3e3f1215.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.3e3f1215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea64b8b88d9fa61c777f",
+    "url": "/portfolio/static/js/main.bb8728ba.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "337e539120620993d6554eb8d84a9186",
+    "url": "/portfolio/static/media/dipl_bak.337e5391.jpg"
+  },
+  {
+    "revision": "07048c330f965a1ac6d4b5b0bb070eef",
+    "url": "/portfolio/static/media/dipl_mag.07048c33.jpg"
+  },
+  {
+    "revision": "5695eb122e9e266b7854fb1e808b0cfd",
+    "url": "/portfolio/static/media/user_icon.5695eb12.jpg"
+  }
+]);
